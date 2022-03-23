5899cf6b4fba0d5ef59cf17647a3067028e2afb4
Damitha Ranasinghe <rmddcr@gmail.com>
2022-03-23 14:09:25 +0530

    fix region

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_subnet" "dmz" {
   vpc_id = aws_vpc.main.id
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   cidr_block = "10.0.10.0/24"
   map_public_ip_on_launch = false
 }
@@ -47,7 +47,7 @@ resource "aws_instance" "ec2" {
   ami  = "ami-0be656e75e69af1a9"
   instance_type = "t3.micro"
   subnet_id  = aws_subnet.dmz.id
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   private_ip = "10.0.10.250"
   root_block_device {
     volume_type = "gp3"
